c063dbb795cd49f91c461c6937d066c09aa4b14d
mmelgies <michalmelgies95@gmail.com>
2024-03-22 14:40:57 +0100

    Fix the addition

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-let checkAge = confirm("Are you under 18?")
+/* FIX CODE ABOVE
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
 
+alert(a + b); // 12*/
 
-let name = prompt("What is your name?");
+//fixed code, added +(shortcout of Number)
 
-let currentYear = new Date().getFullYear();
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
 
-var yearOfBirth = Number(prompt(`Hello ${name}, please enter your year of Birth`));
-
-let userAge = currentYear -yearOfBirth;
-
-
-if (checkAge == true && userAge > 18) {
-    alert(`Welcome! ${name}`)
-}
-    else if (checkAge == true && userAge < 18) {
-        alert(`${name} you are a liar "Woop-woop" Can you hear it? that's the sound of da police! `)
-    }
-    else {
-        alert(`${name} you have to been under 18 to visit this website`)
-    }
-   
-
-
-
-
-
+alert(a + b); 
