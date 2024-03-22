9c3d5a508535f46872f15d310ef02367558932df
mmelgies <michalmelgies95@gmail.com>
2024-03-22 13:41:34 +0100

    create a simple page that asks for a name and outputs it ;)

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-let isBoss = confirm("Are you the boss?");
+let checkAge = confirm("Are you under 18?")
 
-alert( isBoss );
+
+let name = prompt("What is your name?");
+
+let currentYear = new Date().getFullYear();
+
+var yearOfBirth = Number(prompt(`Hello ${name}, please enter your year of Birth`));
+
+let userAge = currentYear -yearOfBirth;
+
+
+if (checkAge == true && userAge > 18) {
+    alert(`Welcome! ${name}`)
+}
+    else if (checkAge == true && userAge < 18) {
+        alert(`${name} you are a liar "Woop-woop" Can you hear it? that's the sound of da police! `)
+    }
+    else {
+        alert(`${name} you have to been under 18 to visit this website`)
+    }
+   
+
+
+
+
+
